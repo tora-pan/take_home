9563019d7c762c8656000df0b9ceaece7a3aa9b1
tora_pan <travis.pandos@gmail.com>
2021-09-19 23:28:28 -0700

    take home project completed up to 2

---
+++ api/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+const csv = require("csv-parser");
+const fs = require("fs");
+
+let results = [];
+const fileName = "properties.csv";
+
+const getData = (file) => {
+  fs.createReadStream(file)
+    .pipe(csv({}))
+    .on("data", (data) => results.push(data))
+    .on("end", () => {});
+  return results;
+};
+
+app.get("/data", (req, res) => {
+  console.log("getting pinged");
+  if (results.length == 0) {
+    results = getData(fileName);
+  }
+  let filtered = [];
+  for (let i = 0; i < results.length; i++) {
+    if (Object.values(results[i]).indexOf("ca") > -1) {
+      filtered.push(results[i]);
+    }
+  }
+  res.send(filtered);
+});
+
+app.listen(5000);
