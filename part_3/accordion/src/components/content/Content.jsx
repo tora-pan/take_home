2f9b63c4784f61d0d5115a558aaf846d4ff7993a
Travis Pandos <travis.pandos@gamaaviation.com>
2021-09-21 14:42:26 -0700

    remove hard coded string and replace with dynamic string

--- part_3/accordion/src/components/content/Content.jsx
+++ part_3/accordion/src/components/content/Content.jsx
@@ -3,12 +3,12 @@ import Panel from "../panel/Panel";
 import "./content.styles.css";
 
 const Content = ({ selected, panelA, panelB }) => {
-  const [activePanels, setActivePanels] = useState(["panelA"]);
+  const [activePanels, setActivePanels] = useState([panelA]);
 
   return (
     <div className={`content-wrapper ${selected ? "visible" : "hidden"}`}>
-      <Panel active={activePanels.includes("PanelA")} panelText={panelA} setActivePanels={setActivePanels} />
-      <Panel active={activePanels.includes("PanelB")} panelText={panelB} setActivePanels={setActivePanels} />
+      <Panel active={activePanels.includes(panelA)} panelText={panelA} setActivePanels={setActivePanels} />
+      <Panel active={activePanels.includes(panelB)} panelText={panelB} setActivePanels={setActivePanels} />
     </div>
   );
 };
