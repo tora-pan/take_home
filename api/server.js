b09514e186d39a10da7a6c407e788b821fe03090
tora_pan <travis.pandos@gmail.com>
2021-10-10 14:07:26 -0700

    testing async await

--- api/server.js
+++ api/server.js
@@ -5,27 +5,44 @@ const csv = require("csv-parser");
 const fs = require("fs");
 
 let results = [];
+let filteredResults = [];
 const fileName = "properties.csv";
 
+let resultsToPass = {};
+
 const getData = (file) => {
+  console.log("calling getData with : ", file);
   fs.createReadStream(file)
     .pipe(csv({}))
     .on("data", (data) => results.push(data))
     .on("end", () => {});
+  console.log("getData :", results.length);
   return results;
 };
 
 app.get("/data", (req, res) => {
-  if (results.length == 0) {
-    results = getData(fileName);
-  }
-  let filtered = [];
-  for (let i = 0; i < results.length; i++) {
-    if (Object.values(results[i]).indexOf("ca") > -1) {
-      filtered.push(results[i]);
-    }
-  }
-  res.send(filtered);
+  console.log(filteredResults.length);
+  //preParseData();
+  res.send(filteredResults);
 });
 
-app.listen(5000);
+async function preParseData() {
+  if (results.length == 0) {
+    console.log("results not set");
+    results = await getData(fileName);
+  }
+  console.log("results is : ", results.length);
+
+  for (let i = 0; i < results.length; i++) {
+    if (Object.values(results[i]).indexOf("ca") > -1) {
+      filteredResults.push(results[i]);
+    }
+  }
+}
+
+app.listen(5000, function () {
+  // setTimeout(function () {
+  //   preParseData();
+  // }, 3000);
+  preParseData();
+});
