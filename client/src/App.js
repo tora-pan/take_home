a00a538279425dd75c147386a10efe47df24ab06
tora_pan <travis.pandos@gmail.com>
2021-10-20 20:42:19 -0700

    fixed typo on button

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
-  const [buttonText, setButtonText] = useState("Loadd Data");
+  const [buttonText, setButtonText] = useState("Load Data");
 
   async function fetchMyAPI() {
     if (data.length === 0) {
